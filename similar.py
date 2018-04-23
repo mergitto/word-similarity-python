640976668fe1eb3633f0227a61a5503a4b3cfd2b
mergitto <mfm17101@bene.fit.ac.jp>
2018-04-23 12:54:33 +0900

    各手法の類似度トップ10表示処理追加

--- similar.py
+++ similar.py
@@ -4,35 +4,37 @@ import pickle
 from natto import MeCab
 import sys
 from calc import Calc
-
-mc = MeCab('-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
+import time
+from parse import parser
 
 with open('./advice_10.pickle', 'rb') as f:
     advice = pickle.load(f)
 
-def parser(text):
-    words = []
-    for n in mc.parse(text, as_nodes=True):
-        node = n.feature.split(',');
-        if node[0] != '助詞' and node[0] != '助動詞' and node[0] != '記号' and node[1] != '数':
-        #if node[0] != '助詞' and node[0] != '助動詞' and node[0] != '記号' and node[1] != '数' and node[0] != '動詞' and node[0] != '副詞':
-            if node[0] == '動詞':
-                words.append(node[6])
-            else:
-                words.append(n.surface)
-    return words
-
 if __name__ == '__main__':
     parse_sentence = parser(sys.argv[1])
+    similarytyDict = {}
 
     for params in advice.values():
-        if params['advice'] is None:
-            continue
-        parse_advice = parser(params['advice'])
-        calculation = Calc()
-        print("報告書No: " + str(params['reportNo']) + " 企業名" + params['companyName'])
-        print("Jaccard: " + str(calculation.jaccard(parse_sentence, parse_advice)))
-        print("Dice:" + str(calculation.dice(parse_sentence, parse_advice)))
-        print("Simpson:" + str(calculation.simpson(parse_sentence, parse_advice)))
-        print("\n")
+        parse_advice = params['advice_divide']
 
+        calculation = Calc()
+        jaccard_num = float(calculation.jaccard(parse_sentence, parse_advice))
+        dice_num = float(calculation.dice(parse_sentence, parse_advice))
+        simpson_num = float(calculation.simpson(parse_sentence, parse_advice)) if len(parse_advice) > 5 else 0
+
+        similarytyDict[params['reportNo']] = {'companyName': params['companyName'], 'similar': {'jaccard': jaccard_num, 'dice': dice_num,'simpson': simpson_num}}
+
+    top10 = sorted(similarytyDict.values(), key=lambda x: x['similar']['jaccard'], reverse=True)[:10]
+    print('Jaccard')
+    for t10 in top10:
+        print(t10['companyName']+ ':' + str(t10['similar']['jaccard']))
+
+    top10 = sorted(similarytyDict.values(), key=lambda x: x['similar']['dice'], reverse=True)[:10]
+    print('\nDice')
+    for t10 in top10:
+        print(t10['companyName']+ ':' + str(t10['similar']['dice']))
+
+    top10 = sorted(similarytyDict.values(), key=lambda x: x['similar']['simpson'], reverse=True)[:10]
+    print('\nSimpson')
+    for t10 in top10:
+        print(t10['companyName']+ ':' + str(t10['similar']['simpson']))
