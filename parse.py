824b1254cc2fe9cd27f56d18ca60b496b7eaafde
mergitto <mfm17101@bene.fit.ac.jp>
2018-04-26 13:14:52 +0900

    parse処理をmecabとjumanppによるものに分けた

--- parse.py
+++ parse.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from natto import MeCab
+def parser_mecab(text):
+    from natto import MeCab
+    mc = MeCab('-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
 
-mc = MeCab('-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
-
-def parser(text):
     words = []
     for n in mc.parse(text, as_nodes=True):
         node = n.feature.split(',');
@@ -16,3 +15,19 @@ def parser(text):
                 words.append(n.surface)
     return words
 
+def parser_juman(text):
+    from pyknp import Jumanpp
+    jumanpp = Jumanpp()
+
+    result = jumanpp.analysis(text)
+    words = []
+
+    for n in result.mrph_list():
+        if n.hinsi != '助詞' and n.hinsi != '助動詞' and n.hinsi != '特殊':
+            if n.hinsi == '動詞':
+                words.append(n.genkei)
+            else:
+                words.append(n.midasi)
+    return words
+
+#print(mrph.midasi, mrph.yomi, mrph.genkei, mrph.hinsi, mrph.bunrui, mrph.katuyou1, mrph.katuyou2, mrph.imis, mrph.repname)
