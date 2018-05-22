f526a4d030269b9570443f1e30e9532e6c6086e0
akagi <mfm17101@bene.fit.ac.jp>
2018-05-22 10:05:09 +0900

    変数名の修正

--- tfidf/tfidf.py
+++ tfidf/tfidf.py
@@ -28,13 +28,13 @@ def gensim_tfidf(advice):
     tfidf = models.TfidfModel(corpus)#, wglobal=new_idf)#, normalize=False)
     # corpus_tfidf[0]で0番目の文書の単語のcorpusのtfidfを表示
     corpus_tfidf = tfidf[corpus]
-    for index, i in enumerate(corpus):
+    for index, corpus_of_each_text in enumerate(corpus):
         if index % 10 == 0:
             print("進捗度: ", str(round((index+1) / len(corpus) * 100, 2)), '%')
 
         advice[index]['tfidf'] = {}
-        for j in i:
-            tfidf_tuple = [ct for ct in corpus_tfidf[index] if ct[0] == j[0]][0]
+        for one_corpus in corpus_of_each_text:
+            tfidf_tuple = [ct for ct in corpus_tfidf[index] if ct[0] == one_corpus[0]][0]
             advice[index]['tfidf'][dictionary[tfidf_tuple[0]]] = tfidf_tuple[1]
 
     return advice
