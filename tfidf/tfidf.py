67af48aa10a3f2b9d7d19cf96ccf17b704ce217e
mergitto <mfm17101@bene.fit.ac.jp>
2018-06-07 14:25:03 +0900

    tfidfの平均を計算してpickle化する処理追加

--- tfidf/tfidf.py
+++ tfidf/tfidf.py
@@ -28,14 +28,22 @@ def gensim_tfidf(advice):
     tfidf = models.TfidfModel(corpus)#, wglobal=new_idf)#, normalize=False)
     # corpus_tfidf[0]で0番目の文書の単語のcorpusのtfidfを表示
     corpus_tfidf = tfidf[corpus]
-    for index, i in enumerate(corpus):
+    for index, corpus_of_each_text in enumerate(corpus):
         if index % 10 == 0:
             print("進捗度: ", str(round((index+1) / len(corpus) * 100, 2)), '%')
 
         advice[index]['tfidf'] = {}
-        for j in i:
-            tfidf_tuple = [ct for ct in corpus_tfidf[index] if ct[0] == j[0]][0]
+        tfidf_vector_sum = 0
+        for one_corpus in corpus_of_each_text:
+            tfidf_tuple = [ct for ct in corpus_tfidf[index] if ct[0] == one_corpus[0]][0]
             advice[index]['tfidf'][dictionary[tfidf_tuple[0]]] = tfidf_tuple[1]
+            tfidf_vector_sum += tfidf_tuple[1]
+
+        if len(corpus_of_each_text) > 0:
+            tfidf_average = tfidf_vector_sum / len(corpus_of_each_text)
+            advice[index]['tfidf_average'] = tfidf_average
+        else:
+            advice[index]['tfidf_average'] = 0
 
     return advice
 
