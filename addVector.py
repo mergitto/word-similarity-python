78ed0024f1c46c1ff2826a4207b1bcaab1c27f87
mergitto <merugitosu@gmail.ccom>
2018-12-04 16:34:12 +0900

    進捗をプログレスバーで表示させる処理追加

--- addVector.py
+++ addVector.py
@@ -13,29 +13,37 @@ import numpy as np
 from const import WORD2VECMODELFILE
 from parse import parser_mecab
 import os
+from tqdm import tqdm
 
 ROOTPATH = os.path.dirname(os.path.abspath(__file__))
 model = Word2Vec.load(WORD2VECMODELFILE)
 
+def get_stop_word():
+    # ストップワードの取得
+    stop_word = urllib.request.urlopen(
+            'http://svn.sourceforge.jp/svnroot/slothlib/CSharp/Version1/SlothLib/NLP/Filter/StopWord/word/Japanese.txt')
+    sw = [line.decode('utf-8').strip() for line in stop_word] # 読み込んだurlから文章を読み込み
+    sw = [ss for ss in sw if not ss==u''] # 空白を削除
+    stop_word.close()
+    return sw
+
+def vector_sum(parse_text):
+    vectorSum = 0 # 文書の単語ごとのベクトルの和を格納する
+    stop_word = get_stop_word()
+    for word in parse_text:
+        if word in stop_word: continue
+        try:
+            vectorSum += model[word]
+        except:
+            pass
+    return vectorSum
+
 def add_vector(text=None):
     parse_text = [parser_mecab(text)]
     results = {}
-
-    # ストップワードの除去しつつベクトルの和を計算
-    f = urllib.request.urlopen('http://svn.sourceforge.jp/svnroot/slothlib/CSharp/Version1/SlothLib/NLP/Filter/StopWord/word/Japanese.txt')
-    sw = [line.decode('utf-8').strip() for line in f] # 読み込んだurlから文章を読み込み
-    sw = [ss for ss in sw if not ss==u''] # 空白を削除
-    f.close()
-    vectorSum = 0 # 文書の単語ごとのベクトルの和を格納する
-    for word in parse_text[0]:
-        if not word in sw:
-            try:
-                vectorSum += model[word]
-            except:
-                pass
+    vectorSum = vector_sum(parse_text[0])
     results['vectorSum'] = vectorSum
     results['vectorLength'] = np.linalg.norm(vectorSum)
-
     return results
 
 # アドバイスデータの正規化とか記号とかの削除
@@ -50,13 +58,14 @@ if __name__ == '__main__':
     with open('./advice.pickle', 'rb') as f:
         sugAd = pickle.load(f)
 
-    for index in sugAd:
+    for index in tqdm(sugAd):
         report = sugAd[index]
         document = document_norm(report['advice'])
         vector_values = add_vector(document)
-        sugAd[index].update({'vectorSum': vector_values['vectorSum']}) # 文書のベクトルの和を加えたデータに更新
-        sugAd[index].update({'vectorLength': vector_values['vectorLength']}) # 文書のベクトルの和を加えたデータに更新
-        print('報告書No:', str(sugAd[index]['reportNo']), 'トピックリスト作成中:',round(index/len(sugAd) * 100, 1),'%')
+        sugAd[index].update({
+                    'vectorSum': vector_values['vectorSum'],
+                    'vectorLength': vector_values['vectorLength']
+                })
 
     with open('./advice_add_vector.pickle', 'wb') as f:
         pickle.dump(sugAd, f)
