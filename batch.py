59e729077f0d4cdbed2f5228f30a42da432f8a2a
mergitto <merugitosu@gmail.ccom>
2018-12-06 10:03:48 +0900

    文言微修正

--- batch.py
+++ batch.py
@@ -3,10 +3,10 @@ from preprocessing import pgsql
 from preprocessing import addVector
 from preprocessing import tfidf
 
-# 機械学習による前学習用のバッチ処理
+# 機械学習による前学習用のデータ追加処理
 from preprocessing import pgsql_evaluations
 from preprocessing import add_values
 
-# random_forestで学習
+# random_forestで学習&モデルの保存
 from preprocessing import main
 
