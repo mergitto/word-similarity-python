d859a4fc5a3a7319d7106886bacd9754784e645a
mergitto <merugitosu@gmail.ccom>
2018-12-06 14:14:07 +0900

    min_impurity_decreaseは今後削除される予定なので設定しないようにする

--- preprocessing/random_forest.py
+++ preprocessing/random_forest.py
@@ -129,8 +129,7 @@ class Tree():
         clf = RandomForestClassifier(
                 bootstrap=True, class_weight=None, criterion='gini',
                 max_depth=max_depth, max_features='auto', max_leaf_nodes=None,
-                min_impurity_split=1e-07, min_samples_leaf=1,
-                min_samples_split=2, min_weight_fraction_leaf=0.0,
+                min_samples_leaf=1, min_samples_split=2, min_weight_fraction_leaf=0.0,
                 n_estimators=n_estimators, n_jobs=1, oob_score=False, random_state=None,
                 verbose=0, warm_start=False)
         return clf
