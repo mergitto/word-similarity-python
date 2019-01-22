e0dd0d15377fc9eb0c330e1bac2015b80be86229
mergitto <merugitosu@gmail.ccom>
2019-01-22 12:23:48 +0900

    feature_importancesは使わないので式から削除

--- sim2json.py
+++ sim2json.py
@@ -113,8 +113,6 @@ def recommend_rate(reports_values, reports):
         shokushuRate = list_checked(reports_values[report_no]["shokushu"], company_shokushu_name)
         if recommend_formula == 1:
             recommend_rate = (simSumNorm[report_no] + reports[report_no]["predicted_high_rate"]) * (typeRate * shokushuRate)
-        elif recommend_formula == 2:
-            recommend_rate = (simSumNorm[report_no] + reports[report_no]["auto_importance_rate_norm"]) * (typeRate * shokushuRate)
         else:
             recommend_rate = simSumNorm[report_no] * (typeRate * shokushuRate)
         compRecommendDic[report_no] = recommend_rate
